7abd09aa67122173cbd4b7f739614590872f4a1c
Fleecean <wsley3epicgames@gmail.com>
2024-06-04 12:59:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
+<?php
+function getUserIpAddr() {
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        // Check IP from internet
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        // Check IP is passed from proxy
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        // Check IP address from remote address
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    echo $ip;
+}
+
+getUserIpAddr();
+?>
 
